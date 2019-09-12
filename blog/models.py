5e765288d7627a88fea1c1c4e52e7ca3ae8cc57e
FetCh86 <fchouder86@gmail.com>
2019-09-13 00:36:06 +0200

    Suppression des fichiers ignorés

--- blog/models.py
+++ blog/models.py
@@ -4,9 +4,10 @@ from django.utils import timezone
 class Article(models.Model):
     titre = models.CharField(max_length=100)
     auteur = models.CharField(max_length=42)
-    contenu = models.TextField(null = True)
+    texte = models.TextField(null = True)
     date = models.DateTimeField(default=timezone.now,
                                 verbose_name="Date de parution")
+    categorie = models.ForeignKey('Categorie', on_delete=models.CASCADE)
 
     class Meta:
         verbose_name="article"
@@ -14,3 +15,9 @@ class Article(models.Model):
 
     def __str__(self):
         return self.titre
+
+class Categorie(models.Model):
+    nom = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.nom
